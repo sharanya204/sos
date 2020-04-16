45dd7ab91ba1013a9aff2cbd7c5a12260d1d1fc5
sharanya204 <41498182+sharanya204@users.noreply.github.com>
2020-04-16 14:43:26 -0700

    Updates

---
+++ components/sections/Drivers.js
@@ -0,0 +1,133 @@
+import React from 'react';
+import styled from 'styled-components';
+import { StaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
+
+import { Section, Container } from '@components/global';
+
+const Drivers = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        art_ivory: file(
+          sourceInstanceName: { eq: "art" }
+          name: { eq: "ivory_desc" }
+        ) {
+          childImageSharp {
+            fluid(maxWidth: 760) {
+              ...GatsbyImageSharpFluid_withWebp_tracedSVG
+            }
+          }
+        }
+
+        art_learn: file(
+          sourceInstanceName: { eq: "art" }
+          name: { eq: "ivory_desc" }
+        ) {
+          childImageSharp {
+            fluid(maxWidth: 760) {
+              ...GatsbyImageSharpFluid_withWebp_tracedSVG
+            }
+          }
+        }
+
+        art_ivory: file(
+          sourceInstanceName: { eq: "art" }
+          name: { eq: "ivory_desc" }
+        ) {
+          childImageSharp {
+            fluid(maxWidth: 760) {
+              ...GatsbyImageSharpFluid_withWebp_tracedSVG
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Section id="drivers">
+        <Container>
+          <Grid>
+            <div>
+              <h2>Medicine</h2>
+              <p>
+              Many advancements in human health are thanks to animals, from laboratory research to extraction of animal parts for their medicinal value.
+              </p>
+            </div>
+            <Art>
+              <Img fluid={data.art_ivory.childImageSharp.fluid} />
+            </Art>
+          </Grid>
+          <Grid inverse>
+            <Art>
+              <Img fluid={data.art_ivory.childImageSharp.fluid} />
+            </Art>
+            <div>
+              <h2>Policy</h2>
+              <p>
+              Policy can help or hurt species populations, whether it’s protecting an animal from extinction or enabling the decline of an animal by destroying its habitat.
+              </p>
+            </div>
+          </Grid>
+          <Grid>
+            <div>
+              <h2>Culture</h2>
+              <p>
+              Animals are often exploited in the name of culture, including commercialization of animal parts and domesticated house pets.
+              </p>
+            </div>
+            <Art>
+              <Img fluid={data.art_ivory.childImageSharp.fluid} />
+            </Art>
+          </Grid>
+        </Container>
+      </Section>
+    )}
+  />
+);
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 3fr 2fr;
+  grid-gap: 40px;
+  text-align: right;
+  align-items: center;
+  justify-items: center;
+  margin: 24px 0;
+
+  ${props =>
+    props.inverse &&
+    `
+    text-align: left;
+    grid-template-columns: 2fr 3fr;
+  `}
+
+  h2 {
+    margin-bottom: 16px;
+  }
+
+  @media (max-width: ${props => props.theme.screen.md}) {
+    grid-template-columns: 1fr;
+    text-align: left;
+    margin-bottom: 96px;
+
+    &:last-child {
+      margin-bottom: 24px;
+    }
+
+    ${props =>
+      props.inverse &&
+      `
+        ${Art} {
+          order: 2;
+        }
+    `}
+  }
+`;
+
+const Art = styled.figure`
+  margin: 0;
+  max-width: 380px;
+  width: 100%;
+`;
+
+export default Drivers;
---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Layout from '@common/Layout';
+import Navbar from '@common/Navbar';
+
+import Header from '@sections/Header';
+import Description from '@sections/Description';
+import Drivers from '@sections/Drivers';
+// import Brands from '@sections/Brands';
+import Team from '@sections/Team';
+// import Faq from '@sections/Faq';
+import Footer from '@sections/Footer';
+
+const IndexPage = () => (
+  <Layout>
+    <Navbar />
+    <Header />
+    <Description />
+    <Drivers />
+    {/* <Brands /> */}
+    <Team />
+    {/* <Faq /> */}
+    <Footer />
+  </Layout>
+);
+
+export default IndexPage;
