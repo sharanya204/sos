b9c133c44e021ac1644dc87cfa21c3bd6be1312c
sharanya204 <41498182+sharanya204@users.noreply.github.com>
2020-04-19 19:50:42 -0700

    minor cosmetic changes

--- src/components/sections/Description.js
+++ src/components/sections/Description.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { StaticQuery, graphql } from 'gatsby';
-import Img from 'gatsby-image';
 
 import { Section, Container } from '@components/global';
 
