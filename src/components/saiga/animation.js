1c7ebe7a6a17c0ce96898545357755a7d00f7ca3
phoebe-lu <phoebelu@berkeley.edu>
2020-04-29 21:52:24 -0700

    added saiga animation figure to page
    need to figure out how to animate

--- src/components/saiga/animation.js
+++ src/components/saiga/animation.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Section, Container } from '@components/global';
+import '@styles/stylesheet.css';
 
 import { ReactComponent as SaigaAnimationIcon} from '@images/Saiga/antelopelayers.svg';
 
@@ -7,13 +8,19 @@ export const SaigaAnimation = () => (
 	<Section>
 		<Container>
 		<div id="WhyImportant">
-            <h3>Why are Saiga Antelope important?</h3>
+            <h3>Why are Saiga Antelope important?</h3></div>
 			<div className="AnimationText">
                 <p><span>Saiga antelope are a key species for the steppe ecosystem.
                     
                 This simple animation depicts the Saiga's relationship with plants and predators of the steppe.</span></p>
             </div>
+			<div className="figure">
+				<SaigaAnimationIcon classname = "figureicon" id = "figuresaiga">
+				</SaigaAnimationIcon>
+			</div>
 		</Container>
 	</Section>
-)
 
+);
+
+export default SaigaAnimation;
--- src/pages/saiga.js
+++ src/pages/saiga.js
@@ -4,14 +4,18 @@ import { Link } from 'gatsby';
 
 
 import { QuickFactsSaiga } from '@sections/QuickFacts.js'
+import { SaigaAnimation } from '@components/saiga/animation.js' 
 import Layout from '@common/Layout';
 import { Section, Container } from '@components/global';
+import Title from '@sections/Title';
 
 const Saiga = () => (
   <Layout>
     <Section id="saigasection">
       {/* <h2 className="sectionheader">Elephant</h2> */}
+      <Title animalname = {"Saiga"}></Title>
       <QuickFactsSaiga />
+      <SaigaAnimation />
     </Section>
   </Layout>
 );
