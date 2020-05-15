b399c18e59fba108e98bb1346062e095cbadba9d
phoebe-lu <phoebelu@berkeley.edu>
2020-05-15 14:37:58 -0700

    update text

--- src/components/saiga/saigaanimation.js
+++ src/components/saiga/saigaanimation.js
@@ -12,7 +12,7 @@ export const SaigaAnimation = () => (
             <h1>Why are Saiga Antelope important?</h1>
 			<br></br>
                 <p><span>Saiga antelope are a key species for the steppe ecosystem.
-				Without the Saiga, plant diversity and predator populations dramatically decrease.
+				Without the Saiga, plant diversity and predator populations dramatically decrease, which ultimately has implications for humans.
                 This simple animation depicts the Saiga's relationship with plants and predators of the steppe.
 				Click <a href = "https://observablehq.com/@phoebe/final-animation">here</a> to access the Observable notebook and interact with the animation.</span></p></div>
 			<div className="figure">
